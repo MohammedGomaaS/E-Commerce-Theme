110ede3d40d9569659d828167607b469c298fa63
Mohammed Gomaa <mohammed.gomaa9484@gmail.com>
2019-01-03 14:27:17 +0200

    readme

---
+++ styles/js/script.js
@@ -0,0 +1,10 @@
+(function(){
+    var waypoints = $('.cards').waypoint(function(direction) {
+      
+        $(".card-item").fadeIn(3000); 
+      }, {
+        offset: '80%'
+      })
+     
+   
+})();
